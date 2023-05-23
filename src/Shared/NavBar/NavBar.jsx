aab7a9bfe6345dda81840fcd9cf2be1e20ed6ded
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-05-23 21:13:55 +0600

    add banner and slider

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -16,7 +16,7 @@ const NavBar = () => {
         <li><a>Item 3</a></li>
     </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar fixed z-10 bg-opacity-60 max-w-screen-xl bg-base-100">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
