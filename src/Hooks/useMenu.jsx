02fcea9c4290b0b834e704233f2e6244b45ee4ba
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-05-24 19:59:07 +0600

    add some button style and fetch data fromm backend server

--- src/Hooks/useMenu.jsx
+++ src/Hooks/useMenu.jsx
@@ -1,19 +1,18 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react';
 
-const useMenu=()=>{
-    const [menu,setMenu]=useState([])
-    const [loading,setLoading]=useState(true)
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data)
-            setLoading(false)
-            // const filter=data.filter(item=>item.category=='popular')
-            setMenu(data)
-
-        })
-    },[])
-    return [menu,loading]
-}
+const useMenu = () => {
+  const [menu, setMenu] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch('http://localhost:5000/menu')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+        // const filter=data.filter(item=>item.category=='popular')
+        setMenu(data);
+      });
+  }, []);
+  return [menu, loading];
+};
 export default useMenu;
--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -7,11 +7,11 @@ const FoodCard = ({item}) => {
         <div className="card w-96 bg-base-100 shadow-xl ">
         <figure><img src={image} /></figure>
           <p className='bg-slate-900 absolute right-0 mr-4 mt-4 px-4'>{price}</p>
-        <div className="card-body text-center">
+        <div className="card-body flex flex-col items-center">
           <h2 className="card-title">{name}</h2>
           <p>{recipe}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Add to Cart</button>
+          <button className="btn btn-outline border-0 border-b-4 text-slate-950 border-orange-400">Add To Cart</button>
           </div>
         </div>
       </div>
