87848ddc66c9207096fb6a7de7136f0d69ea8910
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-05-24 14:28:16 +0600

    add salad soups and pizza section

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -6,6 +6,10 @@ import PopularMenu from '../../Home/PopularManu/PopularMenu';
 import useMenu from '../../../Hooks/useMenu';
 import SectionTitle from '../../../components/SectionTitl/SectionTitle';
 import MenuCategory from '../MenuCategory/MenuCategory';
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg'
+import soupImg from '../../../assets/menu/soup-bg.jpg'
+import saladImg from '../../../assets/menu/salad-bg.jpg'
 const Menu = () => {
     const [menu]=useMenu()
     const dessert=menu.filter(item=>item.category=='dessert')
@@ -20,8 +24,30 @@ const Menu = () => {
         <title>Bistro Boss || Menu</title>
       </Helmet>
       <Cover img={img} title={'Our Menu'}></Cover>
+      {/* main cover */}
       <SectionTitle subHeading={"Don't Miss"} heading={"Today's offer"}></SectionTitle>
+      {/* offered menu items */}
       <MenuCategory items={offered}></MenuCategory>
+      {/* desserts menu items */}
+      <MenuCategory items={dessert}
+      title={'Dessert'}
+      img={dessertImg}
+      ></MenuCategory>
+      {/* pizza       items */}
+      <MenuCategory items={pizza}
+      title={'pizza'}
+      img={pizzaImg}
+      ></MenuCategory>
+      {/* Salad       items */}
+      <MenuCategory items={salad}
+      title={'Salad'}
+      img={saladImg}
+      ></MenuCategory>
+      {/* soups       items */}
+      <MenuCategory items={soup}
+      title={'Soups'}
+      img={soupImg}
+      ></MenuCategory>
     </div>
   );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MenuItem from '../../../Shared/MenuItem/MenuItem';
+import Cover from '../../../Shared/Cover/Cover';
 
-const MenuCategory = ({items}) => {
+const MenuCategory = ({items,img,title}) => {
     return (
-        <div>
-             <div className='grid md:grid-cols-2 gap-10'>
+        <div className='pt-8'>
+             {title && <Cover img={img} title={title}></Cover>}
+             <div className='grid md:grid-cols-2 gap-10 mt-16'>
                 {
                     items.map(item=> <MenuItem key={item._id}
                     item={item}
